95741b5467bc4f40ee50ac63496df206856c6157
Ernesto Alvarez López <alvarez93lopez@gmail.com>
2025-09-11 12:03:42 -0400

    use new initializer tenant middleware

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Ingenius\Storefront\Providers;
 
-use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+use Ingenius\Core\Http\Middleware\InitializeTenancyByDomain;
+use Ingenius\Core\Http\Middleware\PreventAccessFromCentralDomains;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
